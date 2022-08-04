6726d83f9971077ef74c9e2341219d152a014a12
sipesek50 <audywijaya50@gmail.com>
2022-08-05 00:41:02 +0700

    Login kelar

---
+++ resources/js/components/Index.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState, useSyncExternalStore } from "react";
+import ReactDOM from 'react-dom';
+import Login from "./Pages/Login";
+
+async function getToken(){
+    try {
+        const response = await axios("api/users")
+        return await response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function Index() {
+    // const [token, setToken] = useState(async() => await getToken())
+    const [token, setToken] = useState(false)
+
+    // useEffect(async () => {
+    //     setToken(getToken())
+    // })
+
+    return (
+        <>
+        {token ? "dashboard" : <Login token={setToken}/>}
+        </>
+    )
+}
+
+export default Index;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Index />, document.getElementById('app'));
+}
---
+++ resources/js/components/Pages/Login.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { useState } from "react";
+
+async function requestLogin(data){
+    try {
+        const response = await axios.post("api/login", data)
+        // console.warn(await response)
+        const resp = await response.data
+        return resp
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function Login(props){
+
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+    const [res, setRes] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let data = {
+            "email": email,
+            "password": pass
+        }
+        setRes(await requestLogin(data))
+    }
+
+    console.log(res)
+
+    return(
+        <>
+        <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+            <div className="container">
+                <div className="row justify-content-center">
+                <div className="col-lg-6">
+                    <div className="card-group d-block d-md-flex row">
+                    <div className="card col-md-7 p-4 mb-0">
+                        <div className="card-body">
+                        <h1>Login</h1>
+                        <p className="text-medium-emphasis">Sign In to your account</p>
+                        <form onSubmit={handleSubmit}>
+                        <div className="input-group mb-3"><span className="input-group-text">
+                            <svg className="icon">
+                                <use xlinkHref="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                            </svg></span>
+                            <input className="form-control" onChange={e => setEmail(e.target.value)} type="email" placeholder="Email"/>
+                        </div>
+                        <div className="input-group mb-4"><span className="input-group-text">
+                            <svg className="icon">
+                                <use xlinkHref="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                            </svg></span>
+                            <input className="form-control" onChange={e => setPass(e.target.value)} type="password" placeholder="Password"/>
+                        </div>
+                        <div className="row">
+                            <div className="col-6">
+                            <button className="btn btn-link px-0" type="button">Forgot password?</button>
+                            </div>
+                            <div className="col-6 text-end">
+                            <button className="btn btn-primary px-4" type="submit">Login</button>
+                            </div>
+                        </div>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
