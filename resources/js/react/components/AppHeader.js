67e794913bb843c3fbcb86804176a8763a45e34c
feragano <william.ricardo@binus.ac.id>
2022-09-13 02:57:24 +0700

    [feragano] removing unused jsx component, removing console.log

--- resources/js/react/components/AppHeader.js
+++ resources/js/react/components/AppHeader.js
@@ -26,8 +26,6 @@ export function AppHeader({ setMenu, logout }) {
 
     return (
         <>
-            {/* {"AppHeader"} */}
-
             <CHeader position="sticky" className="mb-4">
                 <CContainer fluid>
                     <CHeaderNav className="ms-3">
@@ -45,35 +43,6 @@ export function AppHeader({ setMenu, logout }) {
                                 <CAvatar src={avatar8} size="md" />
                             </CDropdownToggle>
                             <CDropdownMenu className="pt-0" placement="bottom-end">
-                                {/* <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
-                        <CDropdownItem href="#">
-                        <CIcon icon={cilBell} className="me-2" />
-                        Updates
-                        <CBadge color="info" className="ms-2">
-                            42
-                        </CBadge>
-                        </CDropdownItem>
-                        <CDropdownItem href="#">
-                        <CIcon icon={cilEnvelopeOpen} className="me-2" />
-                        Messages
-                        <CBadge color="success" className="ms-2">
-                            42
-                        </CBadge>
-                        </CDropdownItem>
-                        <CDropdownItem href="#">
-                        <CIcon icon={cilTask} className="me-2" />
-                        Tasks
-                        <CBadge color="danger" className="ms-2">
-                            42
-                        </CBadge>
-                        </CDropdownItem>
-                        <CDropdownItem href="#">
-                        <CIcon icon={cilCommentSquare} className="me-2" />
-                        Comments
-                        <CBadge color="warning" className="ms-2">
-                            42
-                        </CBadge>
-                        </CDropdownItem> */}
                                 <CDropdownHeader className="bg-light fw-semibold py-2">Settings</CDropdownHeader>
                                 <CDropdownItem href="#" onClick={() => setMenu('change-password')}>
                                     <CIcon icon={cilUser} className="me-2" />
@@ -83,25 +52,6 @@ export function AppHeader({ setMenu, logout }) {
                                     <CIcon icon={cilSettings} className="me-2" />
                                     Logout
                                 </CDropdownItem>
-                                {/* <CDropdownItem href="#">
-                        <CIcon icon={cilCreditCard} className="me-2" />
-                        Payments
-                        <CBadge color="secondary" className="ms-2">
-                            42
-                        </CBadge>
-                        </CDropdownItem>
-                        <CDropdownItem href="#">
-                        <CIcon icon={cilFile} className="me-2" />
-                        Projects
-                        <CBadge color="primary" className="ms-2">
-                            42
-                        </CBadge>
-                        </CDropdownItem>
-                        <CDropdownDivider />
-                        <CDropdownItem href="#">
-                        <CIcon icon={cilLockLocked} className="me-2" />
-                        Lock Account
-                        </CDropdownItem> */}
                             </CDropdownMenu>
                         </CDropdown>
                     </CHeaderNav>
